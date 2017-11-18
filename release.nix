949f296db983413ce33848a43183bac8d326e3f1
aszlig <aszlig@nix.build>
2017-11-18 01:56:22 +0100

    release.nix: Add a job for the options manual
    This manual only contains all the nixcloud-related options and I'm
    adding this mainly so that the description for our NixOS options are
    validated correctly.
    
    Apart from that this also has the advantage that it's easier to discover
    which options are actually there without having to resort to evalution
    and/or rely on some external service.
    
    Signed-off-by: aszlig <aszlig@nix.build>

--- release.nix
+++ release.nix
@@ -1,7 +1,8 @@
 { system ? builtins.currentSystem, nixpkgs ? <nixpkgs> }:
 
 let
-  lib = import "${nixpkgs}/lib";
+  pkgs = import nixpkgs { inherit system; };
+  inherit (pkgs) lib;
 
 in {
   tests = let
@@ -11,4 +12,56 @@ in {
     inherit system;
     pkgs = import nixpkgs {};
   });
+
+  manual = let
+    modules = import "${pkgs.path}/nixos/lib/eval-config.nix" {
+      modules = [ ./modules ];
+      check = false;
+      inherit system;
+    };
+
+    isNcOpt = opt: lib.head (lib.splitString "." opt.name) == "nixcloud";
+    filterDoc = lib.filter (opt: isNcOpt opt && opt.visible && !opt.internal);
+    filtered = filterDoc (lib.optionAttrSetToDocList modules.options);
+    # XXX: Instead of unsafe discard, let's actually strip the prefix someday.
+    optionsXML = builtins.unsafeDiscardStringContext (builtins.toXML filtered);
+    optionsFile = builtins.toFile "options.xml" optionsXML;
+
+  in pkgs.stdenv.mkDerivation {
+    name = "nixcloud-webservices-options";
+
+    nativeBuildInputs = [ pkgs.libxslt ];
+
+    buildCommand = ''
+      dest="$out/share/doc/nixcloud-webservices"
+      mkdir -p "$dest"
+
+      cat > manual.xml <<XML
+      <book xmlns="http://docbook.org/ns/docbook"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            xmlns:xi="http://www.w3.org/2001/XInclude">
+        <title>NixOS options specific to nixcloud webservices</title>
+        <xi:include href="options-db.xml" />
+      </book>
+      XML
+
+      xsltproc -o options-db.xml \
+        "${pkgs.path}/nixos/doc/manual/options-to-docbook.xsl" \
+        ${lib.escapeShellArg optionsFile}
+
+      xsltproc -o "$dest/index.html" -nonet -xinclude \
+        --param section.autolabel 1 \
+        --param section.label.includes.component.label 1 \
+        --stringparam html.stylesheet style.css \
+        --param xref.with.number.and.title 1 \
+        --stringparam admon.style "" \
+        ${pkgs.docbook5_xsl}/xml/xsl/docbook/xhtml/docbook.xsl \
+        manual.xml
+
+      cp "${pkgs.path}/nixos/doc/manual/style.css" "$dest/style.css"
+
+      mkdir -p "$out/nix-support"
+      echo "doc manual $dest" > "$out/nix-support/hydra-build-products"
+    '';
+  };
 }
