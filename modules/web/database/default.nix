nixcloud/nixcloud-webservices

commit 93d1cda690f289169a792cf4aa41991274f132d1

Fix database startup ordering
This basically just takes out multi-user.target from the wantedBy list
of database.target. The reason is that whenever a service has databases
enabled but doesn't pull in database.target we will run into a problem
because creation of databases will be done without actually starting up
and/or initialising the DBMS.

I've also added a small comment to illustrate the startup sequence to
make it more clear to people wanting to implement their own DBMS
modules.

Signed-off-by: aszlig <aszlig@nix.build>

modules/web/database/default.nix
@@ -219,16 +219,52 @@ in {
   };
 
   config = lib.mkIf (config.enable && config.database != {}) {
+    # Let's assume we have two databases, one is a PostgreSQL database called
+    # "abc" and the second one is a MariaDB database "xyz". The following chart
+    # illustrates the ordering of the targets/services during startup:
+    #
+    #                             network.target
+    #                                   |
+    #                                  \|/
+    #                  .-------- db-server.target -------.
+    #                  |                |                |
+    #                 \|/               |                |
+    #         mysql-initdb.service      |     postgresql-initdb.service
+    #                  |                |                |
+    #                 \|/               |               \|/
+    #            mysql.service          |        postgresql.service
+    #                  | |              |              | |
+    #                  | `--------------|--------------' |
+    #                  |                |                |
+    #                 \|/               |               \|/
+    #         database-abc.service      |       database-xyz.service
+    #                  |                |                |
+    #                 \|/               |               \|/
+    #  database-abc-post-create.service | database-xyz-post-create.service
+    #                  |                |                |
+    #                  |               \|/               |
+    #                  |--------- database.target -------|
+    #                  |                                 |
+    #                 \|/                               \|/
+    #          database-abc.target              database-xyz.target
+    #                  |                                 |
+    #                  `---------------------------------'
+    #
+    # So this means, that every service that pulls in database.target will get
+    # all the databases necessary. Note that mysql.service/postgresql.service
+    # are NOT ordered before database.target, so that we can ensure parallel
+    # creation of these databases. For example if PostgreSQL is still starting
+    # up, MariaDB databases will still be created in the meantime.
     systemd.targets = {
       db-server = {
         description = "Database Management Systems For ${config.uniqueName}";
         after = [ "network.target" ];
-        instance.requiredBy = [ "database.target" ];
       };
       database = {
         description = "Databases For ${config.uniqueName}";
         after = [ "network.target" ];
-        wantedBy = [ "multi-user.target" ];
+        instance.after = [ "db-server.target" ];
+        instance.requires = [ "db-server.target" ];
       };
     } // lib.mapAttrs' (lib.const (dbcfg: {
       name = "database-${dbcfg.name}";
@@ -236,6 +272,7 @@ in {
         description = "Create Database ${dbcfg.name}";
         instance.requiredBy = [ "database.target" ];
         instance.after = [ "db-server.target" ];
+        instance.before = [ "database.target" ];
       };
     })) config.database;
   };

commit 95f232e553bedb32660cd01239c005d286504e9b

database: Re-add dependency on multi-user.target
Regression introduced by me in 93d1cda690f289169a792cf4aa41991274f132d1.

In theory this would be a good idea to leave out multi-user.target but
we don't have any services that pull in database.target directly and
they also shouldn't need to because that's why we can enable or disable
databases.

So I'm adding back the dependency for now until I have found a good
reason not to do so. Apparently I must have had one then but I didn't
note it in the commit message, which is quite an epic failure (given
that I usually write quite detailled commit messages) to say the least.

Signed-off-by: aszlig <aszlig@nix.build>

modules/web/database/default.nix
@@ -244,17 +244,19 @@ in {
     #                  |                |                |
     #                  |               \|/               |
     #                  |--------- database.target -------|
-    #                  |                                 |
-    #                 \|/                               \|/
-    #          database-abc.target              database-xyz.target
-    #                  |                                 |
-    #                  `---------------------------------'
+    #                  |                |                |
+    #                 \|/               |               \|/
+    #          database-abc.target      |       database-xyz.target
+    #                  |                |                |
+    #                  `----------------|----------------'
+    #                                   |
+    #                                  \|/
+    #                           multi-user.target
     #
-    # So this means, that every service that pulls in database.target will get
-    # all the databases necessary. Note that mysql.service/postgresql.service
-    # are NOT ordered before database.target, so that we can ensure parallel
-    # creation of these databases. For example if PostgreSQL is still starting
-    # up, MariaDB databases will still be created in the meantime.
+    # Note that mysql.service/postgresql.service are NOT ordered before
+    # database.target, so that we can ensure parallel creation of these
+    # databases. For example if PostgreSQL is still starting up, MariaDB
+    # databases will still be created in the meantime.
     systemd.targets = {
       db-server = {
         description = "Database Management Systems For ${config.uniqueName}";