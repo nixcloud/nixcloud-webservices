8b22df790355ddc98ca675375e31d0224a43cf85
aszlig <aszlig@nix.build>
2018-05-11 15:08:49 +0200

    core/version: Allow to easily override version
    This is needed in order to inject the right version for
    nixcloud-webservices in our soon-to-be-added channel.
    
    Basically we rustructure it in a way that it always relies on
    config.nixcloud.version instead of the attributes that are available
    within the local let body.
    
    Signed-off-by: aszlig <aszlig@nix.build>

--- modules/core/version.nix
+++ modules/core/version.nix
@@ -4,9 +4,6 @@ let
   gitDir = toString ../../.git;
   hasGitDir = lib.pathIsDirectory gitDir;
   commitId = lib.commitIdFromGitRepo gitDir;
-  abbrevId = lib.substring 0 7 commitId;
-  version = if hasGitDir then commitId else "master";
-  abbrevVersion = lib.optionalString hasGitDir "_${abbrevId}";
 
   nixcloudVersionScript = pkgs.writeScriptBin "nixcloud-version" ''
     #!${pkgs.stdenv.shell}
@@ -16,13 +13,14 @@ let
 in {
   options.nixcloud.version = lib.mkOption {
     type = lib.types.str;
+    default = if hasGitDir then commitId else "master";
     internal = true;
     description = "The git revision of nixcloud-webservices.";
   };
 
-  config.nixcloud.version = version;
   config.environment.systemPackages = lib.singleton nixcloudVersionScript;
   config.system.nixos.versionSuffix = let
     nixpkgsRev = lib.substring 0 7 config.system.nixos.revision;
-  in ".${nixpkgsRev}-nixcloud${abbrevVersion}";
+    abbrevVersion =  lib.substring 0 7 config.nixcloud.version;
+  in ".${nixpkgsRev}-nixcloud_${abbrevVersion}";
 }
