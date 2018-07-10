fb94ab54540552f2954332fdb1b6f5b853ef95d4
aszlig <aszlig@nix.build>
2018-07-10 15:59:37 +0200

    hydra: Allow to run with a separate Nix store
    Unfortunately this got way messier than what I expected it to be.
    
    So let's start with the evaluator, which is basically a C++ wrapper
    which calls a Perl script (which gathers the inputs) and then again the
    final evaluator is in C++ again. While this works to some extent the
    problem here is that the code uses different ways of interacting with
    the Nix store, so there is currently no bullet-proof "Let's just use the
    Nix daemon with a different root and only interact with the daemon".
    
    While I was searching for a way to get the evaluator running smoothly I
    stumbled upon a bunch of legacy operations in Nix which still use direct
    filesystem access instead of going through the store API. So I decided
    to fully chroot the evaluator and use bind mounts to bring in the
    required store paths along with /etc and others.
    
    Up until that point the evaluator worked fine, at least for a while...
    
    Next up was the queue-runner, which at least isn't a mixture of Perl and
    C++, but this time we have a mixture of store_uri and NIX_REMOTE. The
    former is for the *destination* store and the latter is for the store we
    want to use for building. Obviously the implementation is heavily
    tailored for use with Amazon S3, which the official Hydra is using.
    
    Still, even after patching the queue-runner there was still a problem,
    mainly that it expected that <nix/corepkgs> is available *within* our
    separate store.
    
    I then went on to chroot the queue-runner as well, hoping that I could
    simply bind-mount the closures into the separate store and use
    "nix-store --register-validity" to make sure it can lookup corepkgs.
    
    Again, this went wrong as well, because now Nix was trying to change the
    owner/permissions of the bind-mounted store paths, which doesn't work
    because the global Nix store is mounted read-only (at least on most
    NixOS systems), which is a good thing but it balked my plan.
    
    By bind-mounting the global NIX_DATA_DIR into the chroot *outside* of
    the store however I got the queue-runner working within the chroot.
    
    Now to the bad part: The chroot for the queue-runner as well as the
    chroot for the evaluator are based on the same directory. This means
    that as soon as systemd is starting up both in parallel, they will
    randomly fail to start because of race-conditions during creation of
    leading directories for the bind-mounts.
    
    It's not the end of the world, just a bit ugly and as soon as both are
    finished fighting they run fine.
    
    The final part to fix was just the Hydra server, which was using
    "nix-store --dump" to create a NAR of a particular store path. While
    even nix-store supports NIX_REMOTE for changing the store abstraction it
    doesn't work for all subcommands and for --dump it does not but instead
    tries to access the absolute store path and thus reference the global
    store.
    
    I thought I've fixed that by patching Hydra to switch to nix dump-path
    instead, which has a --store flag. However it turns out that neither
    dump-path nor nix-store --dump really communicates with the Nix daemon
    and both try to access the path directly.
    
    For this reason I disabled useSeparateStore by default for now, because
    the binary cache won't work for the separate store yet. We need to
    possibly fix Nix, so that dump-path uses the daemon to stream a NAR.
    
    Signed-off-by: aszlig <aszlig@nix.build>

--- modules/web/services/hydra/default.nix
+++ modules/web/services/hydra/default.nix
@@ -1,7 +1,62 @@
-{ config, toplevel, pkgs, lib, mkUniqueUser, mkUniqueGroup, ... }:
+{ config, toplevel, pkgs, lib, mkUnique, mkUniqueUser, mkUniqueGroup, ... }:
 
 let
+  nix = toplevel.config.nix.package.overrideAttrs (drv: {
+    # This patch allows pass a --store argument to nix-daemon.
+    patches = (drv.patches or []) ++ lib.singleton (pkgs.fetchpatch {
+      url = "https://github.com/NixOS/nix/commit/"
+          + "e388739098cfb14a166923cd24b6140674b176f8.patch";
+      sha256 = "0mh328ff9p6np1iw70lkrvsiwr10y6y3dqj6mq47w0y4n38vimck";
+    });
+
+    # XXX: We can't easily .override nix.perl-bindings, so it's copy & pasted
+    # from <nixpkgs/pkgs/tools/package-management/nix/default.nix>.
+    passthru.perl-bindings = pkgs.stdenv.mkDerivation {
+      name = "nix-perl-" + nix.version;
+
+      inherit (nix) src;
+
+      postUnpack = "sourceRoot=$sourceRoot/perl";
+
+      nativeBuildInputs = [
+        pkgs.perl pkgs.pkgconfig pkgs.curl nix pkgs.libsodium pkgs.boost
+      ];
+
+      configureFlags = with pkgs.perl; [
+        "--with-dbi=${pkgs.perlPackages.DBI}/${libPrefix}"
+        "--with-dbd-sqlite=${pkgs.perlPackages.DBDSQLite}/${libPrefix}"
+      ];
+
+      preConfigure = "export NIX_STATE_DIR=$TMPDIR";
+
+      preBuild = "unset NIX_INDENT_MAKE";
+
+    };
+  });
+
+  gcRootsDir = let
+    mainPath = "/nix/var/nix/gcroots/${mkUnique "hydra"}";
+    pre = lib.optionalString config.useSeparateStore "${config.stateDir}/root";
+  in pre + mainPath;
+
+  # Patch Hydra so that it uses nix dump-path with a --store argument.
+  # Unfortunately we can't simply use the store_uri because that would bypass
+  # the Nix daemon in other places.
+  #
+  # I'm using HYDRA_NIX_STORE_ROOT as an environment variable here so that we
+  # don't need to rebuild Hydra every time we add a new instance.
+  hydra = (config.package.override { inherit nix; }).overrideAttrs (drv: {
+    postPatch = ''
+      substituteInPlace src/lib/Hydra/View/NixNAR.pm \
+        --replace "nix-store --dump" \
+                  "nix dump-path --store '\$ENV{HYDRA_NIX_STORE_ROOT}'"
+    '';
+  });
+
   hydraEnv = {
+    # XXX: Remove this!
+    HYDRA_NIX_STORE_ROOT = "unix://${config.runtimeDir}/nix-daemon.socket";
+
     HYDRA_DBI = "dbi:Pg:dbname=hydra;user=${mkUniqueUser "hydra"};"
               + "host=${config.database.hydra.socketPath}";
     # TODO: gc_roots_dir!
@@ -18,15 +73,21 @@ let
         maybeHttpsPort = mkMaybePort httpsPort 443;
       in if isHttps then maybeHttpsPort else maybeHttpPort;
       baseURI = "${scheme}://${domain}${maybePort}${path}";
-    in pkgs.writeText "hydra.conf" ''
+    in pkgs.writeText "hydra.conf" (''
       using_frontend_proxy = 1
       base_uri = ${baseURI}
       max_servers = 25
       compress_num_threads = 0
       use-substitutes = ${if config.useSubstitutes then "1" else "0"}
-    '';
+      gc_roots_dir = ${gcRootsDir}
+    '' + lib.optionalString config.useSeparateStore ''
+      store_uri = unix://${config.runtimeDir}/nix-daemon.socket
+    '');
     HYDRA_DATA = config.stateDir;
     IN_SYSTEMD = "1"; # To get log severity levels for the journal
+  } // lib.optionalAttrs config.useSeparateStore {
+    NIX_REMOTE = "unix://${config.runtimeDir}/nix-daemon.socket";
+    NIX_STATE_DIR = "${config.stateDir}/root/nix/var/nix";
   };
 
 in {
@@ -35,6 +96,16 @@ in {
       type = lib.types.path;
       default = pkgs.hydra;
       description = "The Hydra package to use.";
+    };
+
+    useSeparateStore = lib.mkOption {
+      type = lib.types.bool;
+      default = false; # TODO: Set to true once store separation is working.
+      description = ''
+        Set this to <literal>true</literal> if you want to have a separate Nix
+        store from the main one in <filename
+        class="directory">/nix/store</filename>.
+      '';
     };
 
     useSubstitutes = lib.mkOption {
@@ -74,136 +145,273 @@ in {
     };
   };
 
-  config = lib.mkIf config.enable {
-    groups.hydra = {};
+  config = lib.mkMerge [
+    (lib.mkIf config.enable {
+      groups.hydra = {};
 
-    users.hydra = {
-      description = "Hydra";
-      group = "hydra";
-      createHome = true;
-      home = config.stateDir;
-    };
-
-    users.hydra-queue-runner = {
-      description = "Hydra queue runner";
-      group = "hydra";
-      createHome = true;
-      home = "${config.stateDir}/queue-runner";
-    };
-
-    users.hydra-www = {
-      description = "Hydra web server";
-      createHome = true;
-      group = "hydra";
-      home = "${config.stateDir}/www";
-    };
-
-    directories.build-logs = {
-      owner = mkUniqueUser "hydra-queue-runner";
-      group = mkUniqueGroup "hydra";
-      permissions.others.noAccess = true;
-    };
-
-    # Hydra only supports PostgreSQL
-    database.hydra.type = "postgresql";
-    database.hydra.owners = [ "hydra-queue-runner" "hydra-www" ];
-
-    # XXX: Make sure to change this for separate Nix instances.
-    toplevel.nix.trustedUsers = [ (mkUniqueGroup "hydra-queue-runner") ];
-
-    systemd.services.hydra-init = {
-      description = "Hydra Initialisation";
-      environment = hydraEnv;
-      instance.after = [ "database.target" ];
-      serviceConfig.ExecStart = "${config.package}/bin/hydra-init";
-      serviceConfig.User = "hydra";
-      serviceConfig.Type = "oneshot";
-      serviceConfig.RemainAfterExit = true;
-
-      postStart = let
-        hashedPw = builtins.hashString "sha1" config.initialAdminPassword;
-      in ''
-        if [ ! -e ${lib.escapeShellArg config.stateDir}/.init-done ]; then
-          ${lib.escapeShellArg config.package}/bin/hydra-create-user \
-            ${lib.escapeShellArg config.initialAdminUser} \
-            --password-hash ${lib.escapeShellArg hashedPw} \
-            --role admin
-          touch ${lib.escapeShellArg config.stateDir}/.init-done
-        fi
-      '';
-    };
-
-    systemd.services.hydra-server = {
-      description = "Hydra Server";
-      wantedBy = [ "multi-user.target" ];
-
-      instance.requires = [ "hydra-init.service" ];
-      instance.after = [ "hydra-init.service" ];
-
-      environment = hydraEnv;
-
-      serviceConfig = {
-        ExecStart = let
-          args = lib.concatMapStringsSep " " lib.escapeShellArg [
-            # FIXME: Let's use sockets here once we have support for them in
-            #        the reverse proxy.
-            "-f" "-h" config.proxyOptions.ip
-            "-p" (toString config.proxyOptions.port)
-            #"--max_spare_servers" "5" # XXX
-            #"--max_servers" "25" # XXX
-          ];
-        in "@${config.package}/bin/hydra-server hydra-server ${args}";
-        User = "hydra-www";
-        Restart = "on-failure";
+      users.hydra = {
+        description = "Hydra";
+        group = "hydra";
+        createHome = true;
+        home = config.stateDir;
       };
-    };
 
-    systemd.services.hydra-queue-runner = {
-      description = "Hydra Queue Runner";
-      wantedBy = [ "multi-user.target" ];
+      users.hydra-queue-runner = {
+        description = "Hydra queue runner";
+        group = "hydra";
+        createHome = true;
+        home = "${config.stateDir}/queue-runner";
+      };
 
-      instance.requires = [ "hydra-init.service" ];
-      instance.after = [ "hydra-init.service" ];
+      users.hydra-www = {
+        description = "Hydra web server";
+        createHome = true;
+        group = "hydra";
+        home = "${config.stateDir}/www";
+      };
 
-      environment = hydraEnv;
-      path = [
-        config.package pkgs.nettools pkgs.openssh pkgs.bzip2
-        toplevel.config.nix.package
+      directories.build-logs = {
+        owner = mkUniqueUser "hydra-queue-runner";
+        group = mkUniqueGroup "hydra";
+        permissions.others.noAccess = true;
+      };
+
+      # Hydra only supports PostgreSQL
+      database.hydra.type = "postgresql";
+      database.hydra.owners = [ "hydra-queue-runner" "hydra-www" ];
+
+      systemd.services.hydra-init = {
+        description = "Hydra Initialisation";
+        environment = hydraEnv;
+        instance.after = [ "database.target" ];
+        serviceConfig.ExecStart = "${hydra}/bin/hydra-init";
+        serviceConfig.User = "hydra";
+        serviceConfig.Type = "oneshot";
+        serviceConfig.RemainAfterExit = true;
+
+        postStart = let
+          hashedPw = builtins.hashString "sha1" config.initialAdminPassword;
+        in ''
+          if [ ! -e ${lib.escapeShellArg config.stateDir}/.init-done ]; then
+            ${lib.escapeShellArg hydra}/bin/hydra-create-user \
+              ${lib.escapeShellArg config.initialAdminUser} \
+              --password-hash ${lib.escapeShellArg hashedPw} \
+              --role admin
+            touch ${lib.escapeShellArg config.stateDir}/.init-done
+          fi
+        '';
+      };
+
+      systemd.services.hydra-server = {
+        description = "Hydra Server";
+        wantedBy = [ "multi-user.target" ];
+
+        instance.requires = [ "hydra-init.service" ];
+        instance.after = [ "hydra-init.service" ];
+
+        environment = hydraEnv;
+
+        serviceConfig = {
+          ExecStart = let
+            args = lib.concatMapStringsSep " " lib.escapeShellArg [
+              # FIXME: Let's use sockets here once we have support for them in
+              #        the reverse proxy.
+              "-f" "-h" config.proxyOptions.ip
+              "-p" (toString config.proxyOptions.port)
+              #"--max_spare_servers" "5" # XXX
+              #"--max_servers" "25" # XXX
+            ];
+          in "@${hydra}/bin/hydra-server hydra-server ${args}";
+          User = "hydra-www";
+          Restart = "on-failure";
+        };
+      };
+
+      systemd.services.hydra-queue-runner = {
+        description = "Hydra Queue Runner";
+        wantedBy = [ "multi-user.target" ];
+
+        instance.requires = [ "hydra-init.service" ];
+        instance.after = [ "hydra-init.service" ];
+
+        environment = hydraEnv;
+        path = [ hydra pkgs.nettools pkgs.openssh pkgs.bzip2 nix ];
+
+        serviceConfig = {
+          ExecStart = "@${hydra}/bin/hydra-queue-runner hydra-queue-runner -v";
+          ExecStopPost = "${hydra}/bin/hydra-queue-runner --unlock";
+          User = "hydra-queue-runner";
+          Restart = "always"; # XXX!
+          WorkingDirectory = "~";
+        } // lib.optionalAttrs config.useSeparateStore {
+          RootDirectory = "${config.stateDir}/root";
+          MountFlags = "private";
+          MountAPIVFS = true;
+          # FIXME: Make more restrictive!
+          BindPaths = [ "/tmp" "/var" "/etc" "/run" ];
+        };
+      };
+
+      # TODO: Make this DRY with the queue runner
+      systemd.services.hydra-evaluator = {
+        description = "Hydra Evaluator";
+        wantedBy = [ "multi-user.target" ];
+
+        instance.requires = [ "hydra-init.service" ];
+        instance.after = [ "hydra-init.service" ];
+
+        path = [ hydra pkgs.nettools pkgs.jq ];
+
+        environment = hydraEnv // lib.optionalAttrs config.useSeparateStore {
+          NIX_DATA_DIR = "/nix-data";
+        };
+
+        serviceConfig = {
+          ExecStart = "@${hydra}/bin/hydra-evaluator hydra-evaluator -v";
+          ExecStopPost = "${hydra}/bin/hydra-evaluator --unlock";
+          User = "hydra";
+          Restart = "always"; # XXX!
+          WorkingDirectory = "~";
+        } // lib.optionalAttrs config.useSeparateStore {
+          RootDirectory = "${config.stateDir}/root";
+          MountFlags = "private";
+          MountAPIVFS = true;
+          # FIXME: Make more restrictive!
+          BindPaths = [ "/tmp" "/var" "/etc" "/run" ];
+          BindReadOnlyPaths = [ "${nix}/share:/nix-data:rbind" ];
+        };
+      };
+
+      tests.wanted = [ ./test.nix ];
+    })
+    (lib.mkIf (config.enable && !config.useSeparateStore) {
+      toplevel.nix.trustedUsers = [ (mkUniqueGroup "hydra-queue-runner") ];
+    })
+    (lib.mkIf (config.enable && config.useSeparateStore) {
+      directories."/".instance.before = [ "hydra-store-init.service" ];
+
+      # Make sure we don't mess with the permissions needed for the Nix daemon.
+      directories.root.instance.before = [ "hydra-store-init.service" ];
+      directories.root.permissions.enableACLs = false;
+      directories.root.permissions.recursive = false;
+
+      systemd.packages = let
+        # Generate a systemd service unit file (will be merged with the NixOS
+        # systemd.service options) that bind mounts all the paths that are part
+        # of the closure of the the given refs.
+        mkBindMounts = refs: serviceName: pkgs.runCommand "bind-mounts" {
+          exportReferencesGraph = lib.concatLists (lib.imap0 (num: ref: [
+            "closure-${toString num}" ref
+          ]) refs);
+          serviceName = mkUnique "hydra-${serviceName}.service";
+        } ''
+          mkdir -p "$out/lib/systemd/system"
+          servicePath="$out/lib/systemd/system/$serviceName"
+          echo '[Service]' > "$servicePath"
+
+          # Collect all the store paths from the closure-* files and remove
+          # duplicates.
+          runtimeDeps="$(sed -ne '
+            p; n; n
+            :cdown
+            /^0*$/b
+            :l; s/0\(X*\)$/X\1/; tl
+            s/^\(X*\)$/9\1/; tdone
+            ${lib.concatMapStrings (num: ''
+              s/${toString num}\(X*\)$/${toString (num - 1)}\1/; tdone
+            '') (lib.range 1 9)}
+            :done
+            y/X/9/
+            x; n; p; x
+            bcdown
+          ' closure-* | sort -u)"
+
+          for dep in $runtimeDeps; do
+            echo "BindReadOnlyPaths=$dep:$dep:rbind"
+          done >> "$servicePath"
+        '';
+      in [
+        (mkBindMounts [
+          hydra nix pkgs.nettools pkgs.jq hydraEnv.HYDRA_CONFIG
+        ] "evaluator")
+        (mkBindMounts [
+          hydra nix pkgs.nettools pkgs.openssh pkgs.bzip2 hydraEnv.HYDRA_CONFIG
+        ] "queue-runner")
       ];
 
-      serviceConfig = {
-        ExecStart = "@${config.package}/bin/hydra-queue-runner"
-                  + " hydra-queue-runner -v";
-        ExecStopPost = "${config.package}/bin/hydra-queue-runner --unlock";
-        User = "hydra-queue-runner";
-        Restart = "always"; # XXX!
-        WorkingDirectory = "~";
+      users = lib.listToAttrs (map (number: {
+        name = "nixbld${toString number}";
+        value.description = "Nix Build User ${toString number}";
+        value.group = "nixbld";
+        value.extraGroups = lib.singleton (mkUniqueGroup "nixbld");
+      }) (lib.range 1 toplevel.config.nix.nrBuildUsers));
+
+      groups.nixbld = {};
+
+      systemd.sockets.nix-daemon = {
+        description = "Socket For Dedicated Nix Daemon";
+        instance.requiredBy = [ "hydra-init.service" ];
+        instance.before = [ "hydra-init.service" ];
+        socketConfig.ListenStream = "${config.runtimeDir}/nix-daemon.socket";
+        unitConfig.DefaultDependencies = false;
       };
-    };
 
-    # TODO: Make this DRY with the queue runner
-    systemd.services.hydra-evaluator = {
-      description = "Hydra Evaluator";
-      wantedBy = [ "multi-user.target" ];
+      systemd.services.nix-daemon = {
+        description = "Dedicated Nix Daemon For Hydra";
+        instance.after = [ "hydra-store-init.service" ];
 
-      instance.requires = [ "hydra-init.service" ];
-      instance.after = [ "hydra-init.service" ];
+        environment.NIX_CONF_DIR = let
+          cfg = toplevel.config.nix;
+        in pkgs.writeTextDir "nix.conf" ''
+          build-users-group = ${mkUniqueGroup "nixbld"}
+          max-jobs = ${toString cfg.maxJobs}
+          cores = ${toString cfg.buildCores}
+          sandbox = true
+          substituters = ${toString cfg.binaryCaches}
+          trusted-substituters = ${toString cfg.trustedBinaryCaches}
+          trusted-public-keys = ${toString cfg.binaryCachePublicKeys}
+          auto-optimise-store = ${lib.boolToString cfg.autoOptimiseStore}
+          require-sigs = ${lib.boolToString cfg.requireSignedBinaryCaches}
+          trusted-users = ${mkUniqueUser "hydra-queue-runner"}
+          builders =
+          extra-sandbox-paths = ${nix}
+        '';
+        environment.CURL_CA_BUNDLE = "/etc/ssl/certs/ca-certificates.crt";
+        environment.NIX_STATE_DIR = "${config.stateDir}/root/nix/var/nix";
 
-      environment = hydraEnv;
-      path = [ pkgs.nettools config.package pkgs.jq ];
-
-      serviceConfig = {
-        ExecStart = "@${config.package}/bin/hydra-evaluator"
-                  + " hydra-evaluator -v";
-        ExecStopPost = "${config.package}/bin/hydra-evaluator --unlock";
-        User = "hydra";
-        Restart = "always"; # XXX!
-        WorkingDirectory = "~";
+        serviceConfig = {
+          ExecStart = "@${nix}/bin/nix-daemon nix-daemon --store "
+                    + "${lib.escapeShellArg "${config.stateDir}/root"}";
+          KillMode = "process";
+          Nice = toplevel.config.nix.daemonNiceLevel;
+          IOSchedulingPriority = toplevel.config.nix.daemonIONiceLevel;
+          LimitNOFILE = 4096;
+        };
       };
-    };
 
-    tests.wanted = [ ./test.nix ];
-  };
+      systemd.services.hydra-store-init = {
+        description = "Initialise Nix Store For Hydra";
+        instance.requiredBy = [ "hydra-init.service" ];
+        instance.before = [ "hydra-init.service" ];
+
+        serviceConfig.Type = "oneshot";
+
+        script = let
+          userGroup = "${mkUniqueUser "hydra"}:${mkUniqueGroup "hydra"}";
+          escRoots = lib.escapeShellArg gcRootsDir;
+        in ''
+          ${nix}/bin/nix-store --init
+          mkdir -p ${escRoots}
+          chown ${lib.escapeShellArg userGroup} ${escRoots}
+          chmod 2775 ${escRoots}
+        '';
+
+        environment.NIX_REMOTE = "${config.stateDir}/root";
+
+        unitConfig.ConditionPathExists = "!${config.stateDir}/root/nix";
+      };
+    })
+  ];
 
   meta.license = lib.licenses.gpl3Plus;
 }
