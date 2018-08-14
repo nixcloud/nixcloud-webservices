abb1ffa784e0fbf0d163f09519f047e9a8dfa251
aszlig <aszlig@nix.build>
2018-08-14 13:02:45 +0200

    mattermost: Refactor config file generation
    Retrieving the Mattermost configuration file via import-from-derivation,
    apart from being problematic in restricted eval mode has the main
    disadvantage that building during evaluation tremendously increases
    evaluation time.
    
    Also so far the configuration merge and validation was a two step
    endeavour, which now is one single step.
    
    During validation I also added a condition against the Mattermost
    version, because only version 4.x segfaults if the validator isn't run
    inside the prefix directory (in our case the main store path for
    Mattermost) so we only need to work around this in 4.x. Instead of
    symlinking the subdirectories of the mattermost derivation, I simply
    cd'ed to it.
    
    I compared the old generated configuration file with the new one by
    abusing fromJSON/toJSON in Nix to make sure the configuration file is
    ordered and piping it into jq to get pretty-printed output. Both the old
    and the new configuration files after this refactor resulted in the same
    content.
    
    Signed-off-by: aszlig <aszlig@nix.build>
    Cc: @qknight

--- modules/web/services/mattermost/default.nix
+++ modules/web/services/mattermost/default.nix
@@ -1,16 +1,86 @@
 { config, pkgs, lib, mkUniqueUser, mkUniqueGroup, ... }:
-with lib;
-{
+
+let
+  inherit (lib) mkOption types;
+
+  path = builtins.toPath "/${config.proxyOptions.domain}/${config.proxyOptions.path}";
+  siteUrl = "${if (config.proxyOptions.https.mode == "on") then "https" else "http"}:/${path}";
+
+  mattermostConfig = {
+    ServiceSettings.SiteURL = "${siteUrl}"; # "https://chat.example.com";
+    ServiceSettings.ListenAddress = "localhost:${toString config.proxyOptions.port}";
+    TeamSettings.SiteName = config.siteName;
+    SqlSettings = {
+      DriverName = "postgres";
+      DataSource = "postgres:///mattermost?host=${config.database.mattermost.socketPath}";
+      # SECURITY/FIXME: hardcoded
+      AtRestEncryptKey = "7rAh6iwQCkV4cA1Gsg3fgGOXJAQ43QVg";
+    };
+    FileSettings = {
+      # SECURITY/FIXME: hardcoded
+      PublicLinkSalt = "A705AklYF8MFDOfcwh3I488G8vtLlVip";
+    };
+    EmailSettings = {
+      SendEmailNotifications = config.EmailSettings.SendEmailNotifications;
+      ConnectionSecurity = "${config.EmailSettings.ConnectionSecurity}";
+      EnableSMTPAuth = config.EmailSettings.EnableSMTPAuth;
+      SMTPPassword = "${config.EmailSettings.SMTPPassword}";
+      SMTPPort = "${toString config.EmailSettings.SMTPPort}";
+      SMTPServer = "${config.EmailSettings.SMTPServer}";
+      SMTPUsername = "${config.EmailSettings.SMTPUsername}";
+      RequireEmailVerification = config.EmailSettings.RequireEmailVerification;
+      # SECURITY/FIXME: hardcoded
+      InviteSalt = "bjlSR4QqkXFBr7TP4oDzlfZmcNuH9YoS";
+      # SECURITY/FIXME: hardcoded
+      PasswordResetSalt = "vZ4DcKyVVRlKHHJpexcuXzojkE5PZ5eL";
+    };
+  };
+
+  # Merge module options with the upstream config file and validate it.
+  cfgFile = pkgs.runCommand "mattermost-config.json" rec {
+    nativeBuildInputs = [ pkgs.mattermost pkgs.jq ];
+    defaultConfig = "${pkgs.mattermost}/config/config.json";
+    moduleConfig = builtins.toJSON config.config;
+    is5orNewer = let
+      inherit (builtins.parseDrvName pkgs.mattermost.name) version;
+    in lib.versionAtLeast version "5.0";
+    mainBinary = if is5orNewer then "mattermost" else"platform";
+  } ''
+    echo -n "$moduleConfig" | jq -s '.[0] * .[1]' "$defaultConfig" - > "$out"
+
+    # Mattermost version 4.x segfaults if it can't find its prefix in $PWD.
+    if [ -z "$is5orNewer" ]; then
+      cd ${lib.escapeShellArg pkgs.mattermost}
+    fi
+
+    if output="$("$mainBinary" config validate -c "$out" 2>&1)"; then
+      echo "=========== mattermost syntax check ============"
+      echo "syntax is ok! YAY! \o/"
+      echo "   -> $out"
+      echo "=========== /mattermost syntax check ==========="
+    else
+      echo "=========== mattermost syntax check fail ==========="
+      echo -e "$output"
+      echo "   -> $out"
+      echo "=========== /mattermost syntax check fail ==========="
+      echo "You need to fix your mattermost configuration!!1!"
+      exit 1
+    fi
+  '';
+
+in {
   options = {
     siteName = mkOption {
       default = "Mattermost";
       description = "Name of the Mattermost instance";
     };
-    extraConfig = mkOption {
+    config = mkOption {
       type = types.attrs;
-      default = { };
+      default = mattermostConfig;
       description = ''
-        Addtional configuration options as Nix attribute set in config.json schema.
+        Configuration options for Mattermost as Nix attribute set in
+        config.json schema. The documentation for these options can be found at
+        <link xlink:href="https://docs.mattermost.com/administration/config-settings.html" />.
       '';
     };
     EmailSettings = {
@@ -81,130 +151,58 @@ with lib;
     maintainers = with lib.maintainers; [ qknight ];
     meta.platforms = lib.platforms.linux;
   };
-  config =
-    let
-      defaultConfig = builtins.fromJSON (readFile "${pkgs.mattermost}/config/config.json");
 
-      path = builtins.toPath "/${config.proxyOptions.domain}/${config.proxyOptions.path}";
-      siteUrl = "${if (config.proxyOptions.https.mode == "on") then "https" else "http"}:/${path}";
+  config = lib.mkIf config.enable {
+    # Re-define the default config, so that override is possible without
+    # lib.mkOptionDefault.
+    config = mattermostConfig;
 
-      mattermostConf = foldl recursiveUpdate defaultConfig
-        [ { ServiceSettings.SiteURL = "${siteUrl}"; # "https://chat.example.com";
-            ServiceSettings.ListenAddress = "localhost:${toString config.proxyOptions.port}";
-            TeamSettings.SiteName = config.siteName;
-            SqlSettings = {
-              DriverName = "postgres";
-              DataSource = "postgres:///mattermost?host=${config.database.mattermost.socketPath}";
-              # SECURITY/FIXME: hardcoded 
-              AtRestEncryptKey = "7rAh6iwQCkV4cA1Gsg3fgGOXJAQ43QVg";
-            };
-            FileSettings = {
-              # SECURITY/FIXME: hardcoded 
-              PublicLinkSalt = "A705AklYF8MFDOfcwh3I488G8vtLlVip";
-            };
-            EmailSettings = {
-              SendEmailNotifications = config.EmailSettings.SendEmailNotifications;
-              ConnectionSecurity = "${config.EmailSettings.ConnectionSecurity}";
-              EnableSMTPAuth = config.EmailSettings.EnableSMTPAuth;
-              SMTPPassword = "${config.EmailSettings.SMTPPassword}";
-              SMTPPort = "${toString config.EmailSettings.SMTPPort}";
-              SMTPServer = "${config.EmailSettings.SMTPServer}";
-              SMTPUsername = "${config.EmailSettings.SMTPUsername}";
-              RequireEmailVerification = config.EmailSettings.RequireEmailVerification;
-              # SECURITY/FIXME: hardcoded 
-              InviteSalt = "bjlSR4QqkXFBr7TP4oDzlfZmcNuH9YoS";
-              # SECURITY/FIXME: hardcoded 
-              PasswordResetSalt = "vZ4DcKyVVRlKHHJpexcuXzojkE5PZ5eL";
-            };
-          }
-          config.extraConfig
-        ];
-      # https://docs.mattermost.com/administration/command-line-tools.html#mattermost-config-validate
-      mattermostConfJSON = pkgs.writeText "mattermost-config-raw.json" (builtins.toJSON mattermostConf);
-      # mattermost config validate
-      checkAndFormatMattermostConfigfile = configFile:
-      pkgs.stdenv.mkDerivation {
-        name = "mattermost_check_config";
-        src = "";
-        buildInputs = with pkgs; [ jq ];
-        phases = [ "installPhase" ];
-        installPhase = ''
-          mkdir $out/
-          cat ${configFile} | jq '.' > $out/mattermost-config-raw.json
-          # next line prevents a segfault if configuration contains issues
-          ln -sf ${pkgs.mattermost}/{bin,fonts,i18n,templates,client} $out
-          cd $out
-          
-          set +e
-          t=$(${pkgs.mattermost}/bin/mattermost-platform config validate -c $out/mattermost-config-raw.json 2>&1)
-          status=$?
-          set -e
-          
-          if [ "$status" != "0" ]; then
-            echo "=========== mattermost syntax check fail ==========="
-            echo -e "$t"
-            echo "   -> $out/mattermost-config-raw.json"
-            echo "=========== /mattermost syntax check fail ==========="
-            echo "You need to fix your mattermost configuration!!1!"
-            exit 1
-          else
-            echo "=========== mattermost syntax check ==========="
-            echo "syntax is ok! YAY! \o/"
-            echo "   -> $out/mattermost-config-raw.json"
-            echo "=========== /mattermost syntax check ==========="
-          fi
-        '';
+    # inject the leaps websocket
+    proxyOptions.websockets = {
+      ws = {
+        subpath = "/api/v4/websocket";
       };
+    };
 
-    in 
-      lib.mkIf config.enable ({
+    users.mattermost = {
+      description = "Mattermost server user";
+      home        = "${config.stateDir}/www";
+      createHome  = true;
+      group       = "mattermost";
+    };
+    groups.mattermost = {};
 
-        # inject the leaps websocket
-        proxyOptions.websockets = {
-          ws = {
-            subpath = "/api/v4/websocket";
+    database.mattermost = {
+      user = "mattermost";
+      owners = [ "mattermost" ];
+      type = "postgresql";
+    };
+
+    systemd.services.mattermost =
+      assert (config.proxyOptions.path == "/") || abort "Mattermost has no support for subdirectories yet, see 'https://mattermost.uservoice.com/forums/306457-general/suggestions/12468372-install-mattermost-in-a-subdirectory'. The path was ${config.proxyOptions.path}";
+        {
+          description = "${config.uniqueName} main service (mattermost)";
+
+          wantedBy      = [ "multi-user.target" ];
+          after         = [ "network.target" ];
+
+          preStart = ''
+            mkdir -p ${config.stateDir}/www/{data,config,logs}
+            ln -sf ${pkgs.mattermost}/{bin,fonts,i18n,templates,client} ${config.stateDir}/www
+            ln -sf ${cfgFile} ${config.stateDir}/www/config/config.json
+          '';
+
+          serviceConfig = {
+            User = "mattermost";
+            Group = "mattermost";
+            Restart = "on-failure";
+            WorkingDirectory = "${config.stateDir}/www";
+            PrivateTmp = true;
+            ExecStart = "${pkgs.mattermost}/bin/mattermost-platform";
+            LimitNOFILE = "49152";
           };
         };
 
-        users.mattermost = {
-          description = "Mattermost server user";
-          home        = "${config.stateDir}/www";
-          createHome  = true;
-          group       = "mattermost";
-        };
-        groups.mattermost = {};
-
-        database.mattermost = {
-          user = "mattermost";
-          owners = [ "mattermost" ];
-          type = "postgresql";
-        };
-
-        systemd.services.mattermost = 
-          assert (config.proxyOptions.path == "/") || abort "Mattermost has no support for subdirectories yet, see 'https://mattermost.uservoice.com/forums/306457-general/suggestions/12468372-install-mattermost-in-a-subdirectory'. The path was ${config.proxyOptions.path}"; 
-            {
-              description = "${config.uniqueName} main service (mattermost)";
-
-              wantedBy      = [ "multi-user.target" ];
-              after         = [ "network.target" ];
-
-              preStart = ''
-                mkdir -p ${config.stateDir}/www/{data,config,logs}
-                ln -sf ${pkgs.mattermost}/{bin,fonts,i18n,templates,client} ${config.stateDir}/www
-                ln -sf ${checkAndFormatMattermostConfigfile mattermostConfJSON}/mattermost-config-raw.json ${config.stateDir}/www/config/config.json
-              '';
-
-              serviceConfig = {
-                User = "mattermost";
-                Group = "mattermost";
-                Restart = "on-failure";
-                WorkingDirectory = "${config.stateDir}/www";
-                PrivateTmp = true;
-                ExecStart = "${pkgs.mattermost}/bin/mattermost-platform";
-                LimitNOFILE = "49152";
-              };
-            };
-
-        tests.wanted = [ ./test.nix ];
-      });
+    tests.wanted = [ ./test.nix ];
+  };
 }
