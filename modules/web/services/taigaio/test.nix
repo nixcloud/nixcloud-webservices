d1a1fa271a6a63d752f9180f88887b29a72b310b
seitz <paul.m.seitz@gmail.com>
2018-08-23 09:23:43 +0000

    added useless test

--- modules/web/services/taigaio/test.nix
+++ modules/web/services/taigaio/test.nix
@@ -1,22 +1,15 @@
 {
-  name = "leaps";
+  name = "taigaio";
 
   machine.nixcloud.reverse-proxy.enable = true;
   machine.nixcloud.reverse-proxy.extendEtcHosts = true;
-  machine.nixcloud.webservices.leaps = {
+  machine.nixcloud.webservices.taigaio = {
     foo.enable = true;
     foo.proxyOptions.TLS = "none";
     foo.proxyOptions.domain = "example.com";
     foo.proxyOptions.http.mode = "on";
     foo.proxyOptions.https.mode = "off";
     foo.proxyOptions.port = 8080;
-
-    bar.enable = true;
-    bar.proxyOptions.TLS = "none";
-    bar.proxyOptions.domain = "example.org";
-    bar.proxyOptions.http.mode = "on";
-    bar.proxyOptions.https.mode = "off";
-    bar.proxyOptions.port = 8081;
   };
 
   testScript = ''
@@ -39,8 +32,8 @@
 
     $machine->waitForUnit('multi-user.target');
     $machine->waitForOpenPort(80);
+    $machine->waitForOpenPort(8080);
     $machine->succeed('curl http://example.com/ | grep -qF leaps_logo.png');
-    $machine->succeed('curl http://example.org/ | grep -qF leaps_logo.png');
 
     ensureOwner "/var/lib/nixcloud/webservices/leaps-foo/www", "leaps-foo";
     ensureGroup "/var/lib/nixcloud/webservices/leaps-foo/www", "leaps-foo";
