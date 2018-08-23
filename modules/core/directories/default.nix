38eeb268847d8e02cfbff5f825bb30ad5e9bd1e5
aszlig <aszlig@nix.build>
2018-08-23 09:04:31 +0200

    directories: Check users and groups during eval
    If a user or group didn't exist we got a runtime error so far. This is
    especially cruicial if we have services that don't directly require a
    directory service unit, so errors like this are going to stay in the
    dark.
    
    Now we're checking the users and groups specified in the directories
    options and if they don't exist an appropriate assertion is thrown.
    
    Of course, this will also throw an assertion if users and groups are
    managed imperatively, but I find it highly unlikely that the directories
    module is used in conjunction with that. Nevertheless if there really is
    a use case for that we can still introduce an option like "checkUsers",
    to turn it off on a by-case basis.
    
    Signed-off-by: aszlig <aszlig@nix.build>

--- modules/core/directories/default.nix
+++ modules/core/directories/default.nix
@@ -9,5 +9,24 @@
         inherit pkgs lib config;
       }) config.nixcloud.directories;
     in lib.listToAttrs (lib.concatLists serviceList);
+
+    assertions = lib.concatLists (lib.mapAttrsToList (path: cfg: let
+      optname = "nixcloud.directories.\"${path}\"";
+      mkUserAssertion = where: username: {
+        assertion = config.users.users ? ${username};
+        message = "The user '${username}' specified in '${optname}.${where}'"
+                + " does not exist.";
+      };
+      mkGroupAssertion = where: groupname: {
+        assertion = config.users.groups ? ${groupname};
+        message = "The group '${groupname}' specified in '${optname}.${where}'"
+                + " does not exist.";
+      };
+      assertions = [
+        (mkUserAssertion "owner" cfg.owner)
+        (mkGroupAssertion "group" cfg.group)
+      ] ++ map (mkUserAssertion "users") (lib.attrNames cfg.users)
+        ++ map (mkGroupAssertion "groups") (lib.attrNames cfg.groups);
+    in assertions) config.nixcloud.directories);
   };
 }
